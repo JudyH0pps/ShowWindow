8ea92b032bb8ec16a793146fd695e1128b70280f
Siyoung Kim <38729379+JudyH0pps@users.noreply.github.com>
2020-08-24 02:39:44 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -186,5 +186,7 @@ blueBtn.addEventListener('click', showBlueItems);
 yellowBtn.addEventListener('click', showYellowItems);
 pinkBtn.addEventListener('click', showPinkItems);
 
-
 showItems();
+
+document.documentElement.addEventListener('touchstart', function (event) {if (event.touches.length > 1) { event.preventDefault(); } }, false);
+var lastTouchEnd = 0; document.documentElement.addEventListener('touchend', function (event) { var now = (new Date()).getTime(); if (now - lastTouchEnd <= 300) { event.preventDefault(); } lastTouchEnd = now; }, false);
